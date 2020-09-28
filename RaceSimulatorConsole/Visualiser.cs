7710e15496b671c8bb0cadf1b4e19bca9ad2307d
Stroopwafe1 <Stroopwafe1@users.noreply.github.com>
2020-09-28 18:17:11 +0200

    Level 4 completed

--- RaceSimulatorConsole/Visualiser.cs
+++ RaceSimulatorConsole/Visualiser.cs
@@ -1,25 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Controller;
 using Model;
 
 namespace RaceSimulator {
 	public static class Visualiser {
-		private static int compass = 0;
-		private static List<GridSquare> GridSquares = new List<GridSquare>();
+		private static int compass;
+		private static List<GridSquare> GridSquares;
 
 		#region graphics
+		//private static readonly string[] _finishHorizontal = { "____", "  I ", "  I ", "‾‾‾‾" };
+		//private static readonly string[] _finishVertical = { "|  |", "|==|", "|  |", "|  |" };
+		//private static readonly string[] _startHorizontal = { "____", "   ]", " ]  ", "‾‾‾‾" };
+		//private static readonly string[] _startVertical = { "|^ |", "|  |", "| ^|", "|  |" };
+		//private static readonly string[] _cornerSW = { "‾‾\\ ", "  \\", "\\  |", "|  |" };
+		//private static readonly string[] _cornerSE = { " /‾‾", "/   ", "|  /", "|  |" };
+		//private static readonly string[] _cornerNW = { "|  |", "/  |", "   /", "__/ " };
+		//private static readonly string[] _cornerNE = { "|  |", "|  \\", "\\   ", " \\__" };
+		//private static readonly string[] _straightHorizontal = { "____", "    ", "    ", "‾‾‾‾" };
+		//private static readonly string[] _straightVertical = { "|  |", "|  |", "|  |", "|  |" };
 
-		private static readonly string[] _finishHorizontal = {"════", "  ░ ", "  ░ ", "════"};
-		private static readonly string[] _finishVertical = {"║  ║", "║  ║", "║≡≡║", "║  ║"};
-		private static readonly string[] _startHorizontal = {"════", " ▌  ", " ▌  ", "════"};
-		private static readonly string[] _startVertical = {"║  ║", "║▄▄║", "║  ║", "║  ║"};
-		private static readonly string[] _cornerSW = {"═╗  ", "  \\╗", "\\  ║", "║  ║"};
-		private static readonly string[] _cornerSE = {"  ╔═", "╔/  ", "║  /", "║  ║"};
-		private static readonly string[] _cornerNW = {"║  ║", "/  ║", "  /╝", "═╝  "};
-		private static readonly string[] _cornerNE = {"║  ║", "║  \\", "╚\\  ", "  ╚═"};
-		private static readonly string[] _straightHorizontal = {"════", "    ", "    ", "════"};
-		private static readonly string[] _straightVertical = {"║  ║", "║  ║", "║  ║", "║  ║"};
+		private static readonly string[] _finishHorizontal = { "════", "  ░ ", "  ░ ", "════" };
+		private static readonly string[] _finishVertical = { "║  ║", "║  ║", "║≡≡║", "║  ║" };
+		private static readonly string[] _startHorizontal = { "════", "  ▄ ", " ▀  ", "════" };
+		private static readonly string[] _startVertical = { "║  ║", "║▄ ║", "║ ▄║", "║  ║" };
+		private static readonly string[] _cornerSW = { "═╗  ", "  \\╗", "\\  ║", "║  ║" };
+		private static readonly string[] _cornerSE = { "  ╔═", "╔/  ", "║  /", "║  ║" };
+		private static readonly string[] _cornerNW = { "║  ║", "/  ║", "  /╝", "═╝  " };
+		private static readonly string[] _cornerNE = { "║  ║", "║  \\", "╚\\  ", "  ╚═" };
+		private static readonly string[] _straightHorizontal = { "════", "    ", "    ", "════" };
+		private static readonly string[] _straightVertical = { "║  ║", "║  ║", "║  ║", "║  ║" };
 		private static readonly string[] _empty = {"    ", "    ", "    ", "    "};
 		
 		/*
@@ -32,7 +43,8 @@ namespace RaceSimulator {
 		#endregion
 		
 		public static void Initialise() {
-			
+			compass = 1;
+			GridSquares = new List<GridSquare>();
 		}
 
 		public static void DrawTrack(Track track) {
@@ -43,6 +55,7 @@ namespace RaceSimulator {
 			Console.WriteLine($"Lowest values in the grid: X: {GridSquare.LowestX}, Y: {GridSquare.LowestY}");
 			MoveGrid(Math.Abs(GridSquare.LowestX) + 1, Math.Abs(GridSquare.LowestY));
 			GridSquares = GridSquares.OrderBy(_square => _square.Y).ToList();
+			GridSquares.ForEach(_square => _square.Section = InsertParticipants(_square.Section, _square.SectionData.Left, _square.SectionData.Right));
 			int maxX = GridSquares.Max(_square => _square.X);
 			int maxY = GridSquares.Max(_square => _square.Y);
 			for (int y = 0; y <= maxY; y++) {
@@ -56,114 +69,73 @@ namespace RaceSimulator {
 			}
 		}
 
-		private static GridSquare GetGridSquare(int x, int y) {
-			GridSquare square = null;
-			try {
-				square = GridSquares.Find(_square => _square.X == x && _square.Y == y);
-			} catch (Exception e) {
-				square = null;
-			}
+		public static string[] InsertParticipants(string[] track, IParticipant leftParticipant, IParticipant rightParticipant) {
+			char initial1 = leftParticipant?.Name[0] ?? ' ';
+			char initial2 = rightParticipant?.Name[0] ?? ' ';
+			string[] returnValue = new [] {
+				track[0],
+				track[1].Replace('▄', initial1),
+				track[2].Replace('▀', initial2),
+				track[3]
+			};
+			return returnValue;
+		}
 
+		private static GridSquare GetGridSquare(int x, int y) {
+			GridSquare square = GridSquares.Find(_square => _square.X == x && _square.Y == y);
 			return square;
 		}
 
-		private static void DrawSection(SectionTypes type) {
-			switch (type) {
-				case SectionTypes.StartGrid:
-					if(compass == 1 || compass == 3)
-						printVariable(_startHorizontal);
-					else 
-						printVariable(_startVertical);
-					break;
-				case SectionTypes.Straight:
-					if(compass == 1 || compass == 3)
-						printVariable(_straightHorizontal);
-					else 
-						printVariable(_straightVertical);
-					break;
-				case SectionTypes.LeftCorner:
-					if(compass == 1)
-						printVariable(_cornerNW);
-					else if(compass == 2)
-						printVariable(_cornerNE);
-					else if(compass == 3)
-						printVariable(_cornerSE);
-					else 
-						printVariable(_cornerSW);
-					compass = (compass - 1) % 4;
-					break;
-				case SectionTypes.RightCorner:
-					if(compass == 1)
-						printVariable(_cornerSW);
-					else if(compass == 2)
-						printVariable(_cornerNW);
-					else if(compass == 3)
-						printVariable(_cornerNE);
-					else 
-						printVariable(_cornerSE);
-					compass = (compass + 1) % 4;
-					break;
-				case SectionTypes.Finish:
-					if(compass == 1 || compass == 3)
-						printVariable(_finishHorizontal);
-					else 
-						printVariable(_finishVertical);
-					break;
-			}
-		}
-
-		private static void printVariable(string[] variable) {
-			foreach (string s in variable) {
-				Console.WriteLine(s);
-			}
-		}
-
 		private static void CalculateGrid(LinkedList<Section> sections) {
+			Race race = Data.CurrentRace;
 			int comp = compass;
 			int x = 0, y = 0;
 			foreach (Section section in sections) {
 				SectionTypes type = section.SectionType;
+				SectionData data = race.GetSectionData(section);
 				Console.WriteLine($"Type: {type} [X,Y]: [{x},{y}]");
 				switch (type) {
 					case SectionTypes.StartGrid:
 						if (comp == 1 || comp == 3)
-							GridSquares.Add(new GridSquare(x, y, _startHorizontal));
+							GridSquares.Add(new GridSquare(x, y, _startHorizontal, data));
 						else 
-							GridSquares.Add(new GridSquare(x, y, _startVertical));
+							GridSquares.Add(new GridSquare(x, y, _startVertical, data));
 						break;
 					case SectionTypes.Straight:
 						if(comp == 1 || comp == 3)
-							GridSquares.Add(new GridSquare(x, y, _straightHorizontal));
+							GridSquares.Add(new GridSquare(x, y, _straightHorizontal, data));
 						else 
-							GridSquares.Add(new GridSquare(x, y, _straightVertical));
+							GridSquares.Add(new GridSquare(x, y, _straightVertical, data));
 						break;
 					case SectionTypes.LeftCorner:
 						if(comp == 1)
-							GridSquares.Add(new GridSquare(x, y, _cornerNW));
+							GridSquares.Add(new GridSquare(x, y, _cornerNW, data));
 						else if(comp == 2)
-							GridSquares.Add(new GridSquare(x, y, _cornerNE));
+							GridSquares.Add(new GridSquare(x, y, _cornerNE, data));
 						else if(comp == 3)
-							GridSquares.Add(new GridSquare(x, y, _cornerSE));
+							GridSquares.Add(new GridSquare(x, y, _cornerSE, data));
 						else 
-							GridSquares.Add(new GridSquare(x, y, _cornerSW));
+							GridSquares.Add(new GridSquare(x, y, _cornerSW, data));
 						comp = (comp - 1) % 4;
+						if (comp < 0)
+							comp = 3;
 						break;
 					case SectionTypes.RightCorner:
 						if(comp == 1)
-							GridSquares.Add(new GridSquare(x, y, _cornerSW));
+							GridSquares.Add(new GridSquare(x, y, _cornerSW, data));
 						else if(comp == 2)
-							GridSquares.Add(new GridSquare(x, y, _cornerNW));
+							GridSquares.Add(new GridSquare(x, y, _cornerNW, data));
 						else if(comp == 3)
-							GridSquares.Add(new GridSquare(x, y, _cornerNE));
+							GridSquares.Add(new GridSquare(x, y, _cornerNE, data));
 						else 
-							GridSquares.Add(new GridSquare(x, y, _cornerSE));
+							GridSquares.Add(new GridSquare(x, y, _cornerSE, data));
 						comp = (comp + 1) % 4;
 						break;
 					case SectionTypes.Finish:
 						if(comp == 1 || comp == 3)
-							GridSquares.Add(new GridSquare(x, y, _finishHorizontal));
+							GridSquares.Add(new GridSquare(x, y, _finishHorizontal, data));
 						else 
-							GridSquares.Add(new GridSquare(x, y, _finishVertical));
+							GridSquares.Add(new GridSquare(x, y, _finishVertical, data));
 						break;
 				}
 				if (comp == 0) {
